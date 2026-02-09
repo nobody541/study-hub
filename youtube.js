0354dfeed063392d5102175d0085a59c89ca71e7
nobody541 <hopvuthupham@gmail.com>
2026-02-09 17:06:46 -0500

    first commit

---
+++ youtube.js
@@ -0,0 +1,5 @@
+const youtube = [
+            { Content: "Fundamentals of Probability", link: "https://youtu.be/3K9vJ_2fgGA", details:"Essential probability concepts including outcomes, events, sample space, and random variables. Explore key topics like dependent, independent, and mutually exclusive events. Multiplication Rule, Bayes' Rule, and the Total Probability Rule, with clear examples." },
+              
+        
+        ];
