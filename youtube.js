d0cc1cb1d781be6cb220795d90ca1621d1254a9d
nobody541 <hopvuthupham@gmail.com>
2026-02-10 08:59:10 -0500

    add more videos

--- youtube.js
+++ youtube.js
@@ -1,5 +1,7 @@
 const youtube = [
             { Content: "Fundamentals of Probability", link: "https://youtu.be/3K9vJ_2fgGA", details:"Essential probability concepts including outcomes, events, sample space, and random variables. Explore key topics like dependent, independent, and mutually exclusive events. Multiplication Rule, Bayes' Rule, and the Total Probability Rule, with clear examples." },
-              
+              {Content: "Daily challenge", link:"https://youtu.be/8ArnDk8IO68", details:"Calculating Probability Venn Diagram Questions: Conditional, Joint, and Union Probabilities"},
+              {Content: "Daily challenge", link:"https://youtu.be/3u49fnfX0Ks", details:"Can Independent events be mutually exclusive?"},
+
         
         ];
